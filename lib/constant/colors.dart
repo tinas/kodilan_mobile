8b91ccd493fabc7bbc888d0f34f97b4a45928597
tinas <ahmettinastepe0@gmail.com>
2021-04-01 16:27:40 +0300

    Implement app colors

---
+++ lib/constant/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const green = Color(0xFF26AE61);
+  static const orange = Color(0xFFF1630D);
+  static const yellow = Color(0xFFDFB634);
+  static const blue = Color(0xFF186FC9);
+  static const background = Color(0xFFF8F8F8);
+  static const placeholder = Color(0xFF999999);
+  static const popupBackground = Color(0xFFFFFF1A);
+  static const tabDisable = Color(0xFF777777);
+  static const tabIcon = Color(0xFF5C5C5C);
+  static const title = Color(0xFF333333);
+}
